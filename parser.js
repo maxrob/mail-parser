baa28b7853aa23814bfa0c1d6f98de8052ad112e
Maxime Robin <maxime.a.robin@gmail.com>
2017-11-08 19:07:10 +0100

    Use class to find pricing

--- parser.js
+++ parser.js
@@ -1,64 +1,18 @@
 import fs from 'fs';
-import htmlparser from 'htmlparser2';
+import cheerio from 'cheerio';
 
 const filename = './mails/mail.html';
 const html = fs.readFileSync(filename, {encoding: 'utf8'});
 const data = [];
 
-const parser = new htmlparser.Parser({
-    ontext: function(text){
-        data.push(text);
-    }
-}, {decodeEntities: false});
-
-parser.write(html);
-parser.end();
-
-let tmpData = [];
-
-// Clear
-
-for (let i=0, l=data.length; i<l; i++) {
-    if (data[i].replace(/ /g,'') !== "" && !data[i].includes("\\r\\n")) {
-        tmpData.push(data[i])
-    }
-}
-
-let prices = [];
-let code = "";
-let name = "";
-let totalPrice = "";
-
-for (let i=0, l=tmpData.length; i<l; i++) {
-
-    // Pricing
-    if(
-        (tmpData[i].replace(/&nbsp;/g,' ').includes("4 passagers ")
-        || tmpData[i].replace(/&nbsp;/g,' ').includes("Carte Enfant+"))
-        && tmpData[i+1].replace(/&nbsp;/g,' ').includes(" €")
-    ) {
-        let price = parseFloat(tmpData[i + 1].match(/([0-9,]+(\\,[0-9]{2})?)/gm)[0].replace(/,/g,'.'));
-        prices.push({"values" : price})
-    }
-
-    // Trips
-
-    if(tmpData[i].replace(/&nbsp;/g,' ').includes("Votre Carte de réduction") && tmpData[i+1].replace(/&nbsp;/g,' ').includes("Référence de dossier :")) {
-        code = tmpData[i + 2].replace(/ /g,'');
-    }
-
-    if(tmpData[i].replace(/&nbsp;/g,' ').includes("Nom associé :")) {
-        name = tmpData[i + 1].replace(/ /g,'');
-    }
-
-    if(tmpData[i].replace(/&nbsp;/g,' ').includes("TOTAL payé en ligne :")) {
-        totalPrice = parseFloat(tmpData[i + 1].match(/([0-9,]+(\\,[0-9]{2})?)/gm)[0].replace(/,/g,'.'));
-    }
-
-    console.log(tmpData[i].replace(/&nbsp;/g,' '));
-}
+const $ = cheerio.load(html);
 
 
-
-
-
+// Get Pricing
+$('.product-header').each((index, element) => {
+    $(element).find('tr > td').each((index2, element2) => {
+        if($(element2).text().includes(" €")) {
+            console.log(parseFloat($(element2).text().match(/([0-9,]+(\\,[0-9]{2})?)/gm)[0].replace(/,/g,'.')))
+        }
+    });
+});
